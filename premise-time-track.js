034e1d3eebfd198f29a1561c490d48380724332b
François Jacquet <francoisjacquet@users.noreply.github.com>
2017-01-13 11:25:59 -0500

    Move authorization process to new tab!

--- premise-time-track.js
+++ premise-time-track.js
@@ -4,6 +4,7 @@
 
 // Global vars.
 var ptt,
+	tabID,
 	phpClientUrl = 'http://tt-wp-api.vallgroup.com/premise-time-track-client'; // Without trailing slash!
 
 // Init on load.
@@ -33,19 +34,33 @@ function init() {
 
 	var url = phpClientUrl;
 
-	if ( Object.keys( ptt ).length > 0 ) {
+	if ( Object.keys( ptt ).length > 1 ) {
 
 		// PTT API saved, get it.
 		url += '?site_base=' + encodeURIComponent( ptt.site_base ) +
 			'&client_key=' + encodeURIComponent( ptt.client_key ) +
 			'&client_secret=' + encodeURIComponent( ptt.client_secret ) +
 			'&token_credentials=' + encodeURIComponent( ptt.token_credentials ); // ?site_base=http%3A%2F%2Flocalhost%2Ftest%2Fpremisesplitview%2F&client_key=I9aT2lBzYE2n&client_secret=0WwKpqHwgoVOgwwI7HgyjdAItd4DLZd8wEIQ2R6eRp0Lvqd8&token_credentials=O%3A49%3A%22League%5COAuth1%5CClient%5CCredentials%5CTokenCredentials%22%3A2%3A%7Bs%3A13%3A%22%00%2A%00identifier%22%3Bs%3A24%3A%229xMnHuPSmJrLKaWlyEDBytRu%22%3Bs%3A9%3A%22%00%2A%00secret%22%3Bs%3A48%3A%22z66bCzlBQX9smdjsx3ROS89ltMq7UZaej6YJ56dC3FmiZDbg%22%3B%7D
+	} else if ( ! ptt.auth_tab ||
+			document.URL.indexOf( 'auth-tab' ) < 0 ) {
+
+		// Move authorization process to new tab!
+		chrome.tabs.create({url: document.URL + '?auth-tab=1'}, function(tab){
+			ptt.auth_tab = tab.id;
+			console.log(ptt.auth_tab);
+			setStoredObject( 'ptt', ptt );
+		});
+
+		return;
+	} else if ( document.URL.indexOf( 'auth-tab' ) > 0 ) {
+
+		$('body').addClass('auth-tab');
 	}
 
 	// Launch Iframe.
 	document.body.innerHTML = '<iframe src="' + url + '" id="ptt-iframe"></iframe>';
 
-	if ( Object.keys( ptt ).length === 0 ) {
+	if ( Object.keys( ptt ).length < 2 ) {
 
 		var iframe = document.getElementById('ptt-iframe');
 
@@ -73,12 +88,21 @@ function receiveMessage(event) {
 	if (phpClientUrl.indexOf(event.origin) !== 0)
 		return;
 
+	var tabID = ptt.auth_tab;
+
 	// event.source is iframe
 	// event.data is "hi there yourself!  the secret response is: rheeeeet!"
 	// console.log(event.data);
 	ptt = event.data;
 
 	localStorage.setItem( 'ptt', ptt );
+
+	// Close tab!
+	chrome.tabs.remove( tabID, function() {
+
+		// Open popup? You can't...
+		// http://stackoverflow.com/questions/10479679/how-can-i-open-my-extensions-pop-up-with-javascript
+	});
 }
 
 
